196eb72231eb1ac9fba83d266077181f64db613c
Raj Chermako <chermakoraj6@gmail.com>
2026-08-26 21:59:37 +0530

    Que1>c-programming

---
+++ 56962-hello-world-in-c-Hackerrank/56962-hello-world-in-c.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char s[100];
+    printf("Hello, World!\n");
+    scanf("%[^\n]%*c", &s);
+    printf("%s",s);
+    	
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
